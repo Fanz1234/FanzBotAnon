a02c2e55eb3e705a74f9ba7d068cdf5ed1060877
Fanz1234 <118971536+Fanz1234@users.noreply.github.com>
2023-05-10 01:18:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-start('ChatBot.py')
+const { Bot, webhookCallback } = require("grammy");
+const express = require("express");
+
+require("dotenv").config();
+
+const bot = new Bot(process.env.BOT_TOKEN);
+
+if (process.env.NODE_ENV === "production") {
+  const app = express();
+  app.use(express.json());
+  app.use(webhookCallback(bot, "express"));
+
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Bot listening on port ${PORT}`);
+  });
+} else {
+  bot.start(ChatBot.py);
+}
