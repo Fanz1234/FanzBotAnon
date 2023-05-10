b4852d5b89c30b0363f0d79fa777de2c701a54ee
Fanz1234 <118971536+Fanz1234@users.noreply.github.com>
2023-05-10 12:51:44 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-TOKEN = os.environ.get("TOKEN", "5784431540:AAEuT5pL0oh87PRjMuByvgs5UdesXakBQHs") # Dapatkan di @botfather
+TOKEN = os.environ.get("TOKEN", "6157673657:AAEzs4ElUVbYbSPU--zgOPV3VFUMCQMN4Oo") # Dapatkan di @botfather
 OWNER = os.environ.get("OWNER", "Monzky411") # jangan dirubah agar tidak error
 GROUP = os.environ.get("GROUP", "") # 
 CHANNEL = os.environ.get("CHANNEL", "") # 
